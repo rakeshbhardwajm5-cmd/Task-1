71be4e610835aa34cfad651145bd20dec0376b89
rakeshbhardwajm5-cmd <rakeshbhardwajm5@gmail.com>
2026-04-24 22:57:37 +0530

    added arduino code

--- README.ino
+++ README.ino
@@ -1,8 +1,10 @@
 //Task-1
-//Temperature  sensor and the display using Arduino 
+//Temperature  sensor and the display using Arduino
+#include <Arduino.h>
 int pin=A3;
+
 void setup() {
-  serial.begin(9600);
+  Serial.begin(9600);
 }
 void loop() {
   int value = analogRead(pin);
