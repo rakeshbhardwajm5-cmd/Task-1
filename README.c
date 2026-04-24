aab70eb815c51dab97a11f9700e019cc01c759b1
rakeshbhardwajm5-cmd <rakeshbhardwajm5@gmail.com>
2026-04-24 22:45:12 +0530

    updated code

--- README.c
+++ README.c
@@ -1,2 +1,11 @@
-# Task-1
-temperature  sensor and the display using Arduino 
+//Task-1
+//Temperature  sensor and the display using Arduino 
+int pin=A3;
+void setup() {
+  serial.begin(9600);
+}
+void loop() {
+  int value = analogRead(pin);
+  Serial.println(value);
+  delay(1000);
+}
