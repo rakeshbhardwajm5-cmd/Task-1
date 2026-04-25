c15be872512ce4cdc4faca6dc118b7b86ab21b16
rakeshbhardwajm5-cmd <rakeshbhardwajm5@gmail.com>
2026-04-25 20:43:24 +0530

    My latest change

--- README.ino
+++ README.ino
@@ -1,13 +1,47 @@
 //Task-1
 //Temperature  sensor and the display using Arduino
-#include <Arduino.h>
-int pin=A3;
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <DHT.h>
+
+#define DHTPIN 12
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+// Address usually 0x27 or 0x3F
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup() {
-  Serial.begin(9600);
+  lcd.init();        
+  lcd.backlight();  
+  dht.begin();
+
+  lcd.setCursor(0, 0);
+  lcd.print("Temp Monitor");
+  delay(2000);
+  lcd.clear();
 }
+
 void loop() {
-  int value = analogRead(pin);
-  Serial.println(value);
-  delay(1000);
+  float temp = dht.readTemperature();
+  float hum = dht.readHumidity();
+
+  if (isnan(temp) || isnan(hum)) {
+    lcd.setCursor(0, 0);
+    lcd.print("Sensor Error");
+    return;
+  }
+
+  lcd.setCursor(0, 0);
+  lcd.print("Temp: ");
+  lcd.print(temp);
+  lcd.print(" C");
+
+  lcd.setCursor(0, 1);
+  lcd.print("Hum: ");
+  lcd.print(hum);
+  lcd.print(" %");
+
+  delay(2000);
 }
